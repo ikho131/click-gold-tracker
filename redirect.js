23891cac7dfa5a272fdfb5b16c0c69684aba4466
ikho131 <ikho.3line@gmail.com>
2025-04-06 21:10:18 +0900

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,1 +1,20 @@
+export default async function handler(req, res) {
+  const { email, link, university, company } = req.query;
+
+  await fetch("https://hooks.zapier.com/hooks/catch/XXXXXXX/YYYYYYY/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      type: "click",
+      email,
+      link,
+      university,
+      company,
+      time: new Date().toISOString(),
+    }),
+  });
+
+  res.writeHead(302, { Location: decodeURIComponent(link) });
+  res.end();
+}
 
